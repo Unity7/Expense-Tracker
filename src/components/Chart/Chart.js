4b0843cdde3ae05396345624494e7730f1003a82
Bay Bui <baybui@ucla.edu>
2022-09-17 15:13:09 -0700

    added logic for Chart.js

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -5,7 +5,12 @@ const Chart = () => {
   return (
     <div className="chart">
       {props.dataPoint.map((dataPoint) => {
-        <CharBar />;
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />;
       })}
     </div>
   );
